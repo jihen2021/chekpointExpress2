bd86bd123bb7410b8f9d357f41b254892ea19b90
jihen2021 <tun.desc3@gmail.com>
2021-09-11 13:44:38 +0100

    update

--- server.js
+++ server.js
@@ -6,41 +6,41 @@ let app=express()
 app.set('view engine','ejs')
 
 //route static
-//app.use(express.static(__dirname,'public','views','pages'))
+app.use(express.static(__dirname,'public','views','pages'))
 //route 
 
-app.get('/',(req,res)=>
-{
-    //res.send('salut') pas de ejs
-    res.render('pages/index')
-})
-app.get('/contact',(req,res)=>
-{
-    //res.send('salut') pas de ejs
-    res.render('pages/contact')
-})
+// app.get('/',(req,res)=>
+// {
+//     //res.send('salut') pas de ejs
+//     res.render('pages/index')
+// })
+// app.get('/contact',(req,res)=>
+// {
+//     //res.send('salut') pas de ejs
+//     res.render('pages/contact')
+// })
 
-app.get('/ourService',(req,res)=>
-{
-    //res.send('salut') pas de ejs
-    res.render('pages/ourService',{test :'salut'})
-})
-// post contact
-app.post('/pages/contact',(req,res)=>
-{
-// poster un message
+// app.get('/ourService',(req,res)=>
+// {
+//     //res.send('salut') pas de ejs
+//     res.render('pages/ourService',{test :'salut'})
+// })
+// // post contact
+// app.post('/pages/contact',(req,res)=>
+// {
+// // poster un message
 
-})
-  // middle ware pour la gestion de tepm de req
+// })
+//   // middle ware pour la gestion de tepm de req
 
-app.get('/now',(req,res,next)=>
-{
-    req.time = new Date().toString()
-    next()
+// app.get('/now',(req,res,next)=>
+// {
+//     req.time = new Date().toString()
+//     next()
 
-},(req,res)=>{
-    //return
-  res.json({'time': req.time})
-})
+// },(req,res)=>{
+//     //return
+//   res.json({'time': req.time})
+// })
 
 app.listen(3000)
